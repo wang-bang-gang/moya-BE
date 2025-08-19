59202cdc607901453ee529dde64df0a2eb69d3f4
str-leshs <0805eslee@gmail.com>
2025-08-20 01:07:10 +0900

    init: Spring Boot moya project 생성

---
+++ src/main/java/com/moya/MoyaApplication.java
@@ -0,0 +1,13 @@
+package com.moya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moya/MoyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
